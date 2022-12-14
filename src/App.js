7525d99785aeb5f6278f7784d06d9c4d2eb1333e
Bruno Machado <brunomachado3818@gmail.com>
2022-12-14 15:40:37 -0300

    adicionado estilos gerais e estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+
 function App() {
-  return <div>Configurações Iniciais</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
