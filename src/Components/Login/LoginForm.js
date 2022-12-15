6516375e0593c8d7876ec5ed1090dbe882d49619
Bruno Machado <brunomachado3818@gmail.com>
2022-12-15 11:53:01 -0300

    Adicionado os componentes de formulario button e input

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 import styles from "./LoginForm.module.css";
 
 function LoginForm() {
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className={styles.form}>
       <h1>Login</h1> <br />
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" />
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
+      </form>
       <Link to="/login/criar">cadastro</Link>
     </div>
   );
