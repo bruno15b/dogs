bc9b062e0217a18e3f7cb1f23974406d368b4bb2
Bruno Machado <brunomachado3818@gmail.com>
2022-12-22 12:09:17 -0300

    Adicionado estilização e  animação para o componente loading

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "./Home.module.css";
 import Feed from "./Feed/Feed";
+
 function Home() {
   return (
     <section className="container mainContainer">
